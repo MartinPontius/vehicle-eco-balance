3008e0cae5c204f375e66ff7651f55c8a5430aab
Martin Pontius <m.pontius@52north.org>
2020-10-22 10:33:38 +0200

    Add __str__() to Car class

--- vehicle_eco_balance/vehicle/vehicle.py
+++ vehicle_eco_balance/vehicle/vehicle.py
@@ -23,15 +23,19 @@ class Vehicle:
 
 
 class Car(Vehicle):
-    def __init__(self, mass=1500, car_cross_sectional=2.6, air_drag_coefficient=0.3, fuel_type='gasoline', idle_power=2):
+    def __init__(self, mass=1500, car_cross_section=2.6, air_drag_coefficient=0.3, fuel_type='gasoline', idle_power=2):
         super().__init__(mass)
         self.fuel_type = fuel_type
-        self.A = car_cross_sectional    # in m²
+        self.A = car_cross_section      # in m²
         self.cw = air_drag_coefficient  # dimensionless
         self.calorific_value = fuel_types.get(self.fuel_type).get('calorific_value')  # in kWh/l
         self.idle_power = idle_power    # in kW
         self.min_efficiency = fuel_types.get(self.fuel_type).get('min_efficiency')
         self.max_efficiency = fuel_types.get(self.fuel_type).get('max_efficiency')
+
+    def __str__(self):
+        return "Car properties: \n fuel_type: {} \n A: {} \n cw: {} \n calorific_value: {} \n idle_power: {} \n min_efficiency: {} \n max_efficiency: {}".format(
+            self.fuel_type, self.A, self.cw, self.calorific_value, self.idle_power, self.min_efficiency, self.max_efficiency)
 
 class Airplane(Vehicle):
     pass
